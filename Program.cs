b65422468bc5ab1b78abbe73a6d614b260ae317e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-28 23:25:47 -0300

    alteração do program com personalização e publicação do swagger

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,16 +7,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Dummy Data Generator API",
+        Description = "Projeto destinado a portfólio. Totalmente gratuito, este projeto foi pensado para auxiliar programadores e desenvolvedores de sistemas a realizar testes durante o processo de criação de um software online.",
+        Contact = new OpenApiContact
+        {
+            Name = "Romulo de Oliveira",
+            Email = "dev@romulodeoliveira.net",
+            Url = new Uri("https://romulodeoliveira.net/"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Licença",
+            Url = new Uri("https://github.com/romulodeoliveira/DummyDataGenerator-Dotnet6/blob/main/LICENSE.md"),
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
